addc8cc1d9f20f412d64c168e7ae00ea109a95e8
unknown <dingxichen@iqiyi.pps>
2017-02-13 18:09:16 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 #coding=utf-8
 
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='gym_gomoku',
@@ -9,7 +10,22 @@ setup(
     description = 'Game Gomoku or Five-In-a-Row Gym Environment',
     author = 'Xichen Ding',
     author_email = 'dingo0927@126.com',
+    url = 'https://github.com/rockingdingo/gym-gomoku',
     license="MIT",
     keywords='gym gomoku reinforcement learning',
+    packages=find_packages(),
+    package_data={
+        'demo': ['gym_gomoku_demo.gif', 
+            'gym_gomoku_demo.json',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
     install_requires=['gym'],
 )
